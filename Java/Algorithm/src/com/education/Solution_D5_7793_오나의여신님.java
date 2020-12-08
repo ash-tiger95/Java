fb4c15e5a43fd2ccb6d9b4e604f3ef86337261a8
SungHoAhn <jugiaro95@gmail.com>
2020-12-08 23:36:16 +0900

    update

--- Java/Algorithm/src/com/education/Solution_D5_7793_오나의여신님.java
+++ Java/Algorithm/src/com/education/Solution_D5_7793_오나의여신님.java
@@ -1,9 +1,12 @@
 package com.education;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution_D5_7793_오나의여신님 {
 
 	public static void main(String[] args) {
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
