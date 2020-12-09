f9f480b5aa069a3897b889f9ec3f7a2273b40ef9
SungHoAhn <jugiaro95@gmail.com>
2020-12-09 23:03:40 +0900

    update 11:03

--- Java/Algorithm/src/com/education/Solution_D4_1251_하나로.java
+++ Java/Algorithm/src/com/education/Solution_D4_1251_하나로.java
@@ -1,7 +1,11 @@
 package com.education;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution_D4_1251_하나로 {
 	public static void main(String[] args) {
 		// test
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
