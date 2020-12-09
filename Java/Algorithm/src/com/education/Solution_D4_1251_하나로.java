afa288df1553e33d2d4d4dcd29dcac4d7f634642
SungHoAhn <jugiaro95@gmail.com>
2020-12-09 23:08:39 +0900

    commit

--- Java/Algorithm/src/com/education/Solution_D4_1251_하나로.java
+++ Java/Algorithm/src/com/education/Solution_D4_1251_하나로.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 
 public class Solution_D4_1251_하나로 {
 	public static void main(String[] args) {
-		// test
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
