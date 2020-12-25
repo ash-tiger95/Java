647147b66c1922e461f53e6e7617c2627a8954e9
SungHoAhn <jugiaro95@gmail.com>
2020-12-25 22:53:37 +0900

    :o: boj gold4 1504

--- Java/Algorithm/src/com/boj/dijkstra/Main_Gold4_1261_알고스팟.java
+++ Java/Algorithm/src/com/boj/dijkstra/Main_Gold4_1261_알고스팟.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -24,8 +26,9 @@ public class Main_Gold4_1261_알고스팟 {
 		}
 
 	}
-	static class Edge implements Comparable<Edge>{
-		int v,weight;
+
+	static class Edge implements Comparable<Edge> {
+		int v, weight;
 
 		public Edge(int v, int weight) {
 			super();
@@ -42,7 +45,7 @@ public class Main_Gold4_1261_알고스팟 {
 		public int compareTo(Edge o) {
 			return Integer.compare(this.weight, o.weight);
 		}
-		
+
 	}
 
 	static int N, M;
@@ -50,7 +53,7 @@ public class Main_Gold4_1261_알고스팟 {
 	static int[][] room;
 
 	static int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-	
+
 	static ArrayList<Edge>[] list;
 
 	public static void main(String[] args) throws IOException {
@@ -66,37 +69,101 @@ public class Main_Gold4_1261_알고스팟 {
 				map[i][j] = input.charAt(j) - '0';
 			}
 		}
-		print( map);
+		print(map);
 
 		int roomNum = 1;
 		for (int i = 0; i < M; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] == 0 && room[i][j] ==0) {
-					
-					numbering(i, j,roomNum++);
+				if (map[i][j] == 0 && room[i][j] == 0) {
+					numbering(i, j, roomNum++);
 				}
 			}
 		}
 		System.out.println(roomNum);
 		System.out.println("--------");
 		print(room);
-		
+
 		// 그래프 만들기
-		list = new ArrayList[roomNum-1];
-		for(int i=0;i<roomNum-1;i++) {
+		list = new ArrayList[roomNum]; // 총 방 개수, roomNum-1개
+		for (int i = 0; i < roomNum; i++) {
 			list[i] = new ArrayList<Edge>();
 		}
-		
-		boolean[] check = new boolean[roomNum-1];
-		for(int i=0;i<M;i++) {
-			for(int j=0;j<N;j++) {
-				if(room[i][j] != 0 && !check[room[i][j]]) {
-					
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (room[i][j] != 0) {
+//					System.out.println("standard: "+i+" "+j+"--------------");
+					for (int d = 0; d < 4; d++) {
+						int ny = i + dirs[d][0];
+						int nx = j + dirs[d][1];
+
+						if (!boundary(ny, nx) || room[i][j] == room[ny][nx]) { // 경계선 밖으로 나가거나 같은 방이면 패스
+							continue;
+						}
+
+						int pow = 2;
+//						System.out.println("start: ("+i+", "+j+") ("+ny+", "+nx+")");
+						while (true) {
+							ny = i + dirs[d][0] * pow;
+							nx = j + dirs[d][1] * pow;
+							if(!boundary(ny,nx)) {
+								break;
+							}
+							
+							if(room[i][j] == room[ny][nx]) {
+								break;
+							}
+
+							if (room[ny][nx] != room[i][j] && room[ny][nx] !=0) {
+//								System.out.println("in: ("+i+", "+j+") ("+ny+", "+nx+")");
+								list[room[i][j]].add(new Edge(room[ny][nx], pow-1));
+								break;
+							}
+
+							pow++;
+						}
+					}
 				}
 			}
 		}
-		
 
+		for (int i = 0; i < roomNum; i++) {
+			System.out.println(list[i].toString());
+		}
+		
+		int start = room[0][0];
+		int end = room[M-1][N-1];
+		System.out.println(dijkstra(start,end,roomNum));
+
+	}
+
+	private static int dijkstra(int start, int end,int roomNum) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[roomNum];
+		int[] distance = new int[roomNum];
+		
+		pq.add(new Edge(start,0));
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Edge ce = pq.poll();
+			
+			if(visited[ce.v]) {
+				continue;
+			}
+			
+			for(Edge next : list[ce.v]) {
+				if(distance[next.v] > distance[ce.v] + next.weight) {
+					distance[next.v] = distance[ce.v] + next.weight;
+					pq.add(new Edge(next.v,distance[next.v]));
+				}
+			}
+			
+			visited[ce.v] = true;
+		}
+		
+		return distance[end];
 	}
 
 	private static void numbering(int i, int j, int roomNum) {
@@ -119,13 +186,17 @@ public class Main_Gold4_1261_알고스팟 {
 				if (visited[ny][nx])
 					continue;
 
-				if(map[ny][nx] == 0) {
-					queue.offer(new Pair(ny,nx));
+				if (map[ny][nx] == 0) {
+					queue.offer(new Pair(ny, nx));
 					visited[ny][nx] = true;
 					room[ny][nx] = roomNum;
 				}
 			}
 		}
+	}
+
+	private static boolean boundary(int ny, int nx) {
+		return ny >= 0 && nx >= 0 && ny < M && nx < N;
 	}
 
 	private static void print(int[][] map) {
