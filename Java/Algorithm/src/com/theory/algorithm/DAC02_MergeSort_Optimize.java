3963e905bdacc3ddb9f8050ec2a38fb4945470c0
SungHoAhn <jugiaro95@gmail.com>
2020-12-28 15:06:58 +0900

    :o: divide and conquer theory

--- Java/Algorithm/src/com/theory/algorithm/DAC02_MergeSort_Optimize.java
+++ Java/Algorithm/src/com/theory/algorithm/DAC02_MergeSort_Optimize.java
@@ -9,44 +9,44 @@ public class DAC02_MergeSort_Optimize {
 		mergeSort(input);
 		System.out.println(Arrays.toString(input));
 	}
-	
+
 	public static void mergeSort(int[] arr) {
-        sort(arr, 0, arr.length);
-    }
+		sort(arr, 0, arr.length);
+	}
 
-    private static void sort(int[] arr, int low, int high) {
-        if (high - low < 2) {
-            return;
-        }
+	private static void sort(int[] arr, int low, int high) {
+		if (high - low < 2) {
+			return;
+		}
 
-        int mid = (low + high) / 2;
-        sort(arr, 0, mid);
-        sort(arr, mid, high);
-        merge(arr, low, mid, high);
-    }
+		int mid = (low + high) / 2;
+		sort(arr, 0, mid);
+		sort(arr, mid, high);
+		merge(arr, low, mid, high);
+	}
 
-    private static void merge(int[] arr, int low, int mid, int high) { // In-place Algorithm
-        int[] temp = new int[high - low];
-        int t = 0, l = low, h = mid; // index를 위함
+	private static void merge(int[] arr, int low, int mid, int high) { // In-place Algorithm
+		int[] temp = new int[high - low];
+		int t = 0, l = low, h = mid; // index를 위함
 
-        while (l < mid && h < high) {
-            if (arr[l] < arr[h]) {
-                temp[t++] = arr[l++];
-            } else {
-                temp[t++] = arr[h++];
-            }
-        }
+		while (l < mid && h < high) {
+			if (arr[l] < arr[h]) {
+				temp[t++] = arr[l++];
+			} else {
+				temp[t++] = arr[h++];
+			}
+		}
 
-        while (l < mid) {
-            temp[t++] = arr[l++];
-        }
+		while (l < mid) {
+			temp[t++] = arr[l++];
+		}
 
-        while (h < high) {
-            temp[t++] = arr[h++];
-        }
+		while (h < high) {
+			temp[t++] = arr[h++];
+		}
 
-        for (int i = low; i < high; i++) {
-            arr[i] = temp[i - low];
-        }
-    }
+		for (int i = low; i < high; i++) {
+			arr[i] = temp[i - low];
+		}
+	}
 }
