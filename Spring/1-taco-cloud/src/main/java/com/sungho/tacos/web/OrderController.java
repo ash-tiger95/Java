9eab3eb52124245209d165cd8fecfa96459e78b6
SungHoAhn <jugiaro95@gmail.com>
2021-01-14 20:24:41 +0900

    kmp theory

--- Spring/1-taco-cloud/src/main/java/com/sungho/tacos/web/OrderController.java
+++ Spring/1-taco-cloud/src/main/java/com/sungho/tacos/web/OrderController.java
@@ -3,6 +3,7 @@ package com.sungho.tacos.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sungho.tacos.Order;
@@ -13,11 +14,18 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
-	
+
 	@GetMapping("/current")
-	  public String orderForm(Model model) {
-	    model.addAttribute("order", new Order());
-	    return "orderForm";
-	  }
+	public String orderForm(Model model) {
+		model.addAttribute("order", new Order());
+		return "orderForm";
+	}
+	
+
+	@PostMapping
+	public String processOrder(Order order) {
+		log.info("Order submitted: " + order);
+		return "redirect:/";
+	}
 
 }
