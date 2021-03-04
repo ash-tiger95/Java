4ef8b1c8381a5b4d14e15caf9358e914158ba844
SungHoAhn <jugiaro95@gmail.com>
2021-03-04 13:05:32 +0900

    :o: programmers level1 크레인인형뽑기게임

--- Java/Algorithm/src/com/programmers/Solution_level1_크레인인형뽑기게임.java
+++ Java/Algorithm/src/com/programmers/Solution_level1_크레인인형뽑기게임.java
@@ -9,9 +9,6 @@ public class Solution_level1_크레인인형뽑기게임 {
 		int[][] board = { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 3 }, { 0, 2, 5, 0, 1 }, { 4, 2, 4, 4, 2 },
 				{ 3, 5, 1, 3, 1 } };
 		int[] moves = { 1, 5, 3, 5, 1, 2, 1, 4 };
-		System.out.println("input");
-		print(board, board.length);
-		System.out.println();
 		System.out.println(solution(board, moves));
 	}
 
