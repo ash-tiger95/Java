5e7695c855a17edc4a86c73fffbdfd6caf4a7683
SungHoAhn <jugiaro95@gmail.com>
2021-03-04 13:21:41 +0900

    :o: programmers level1 두개뽑아서더하기

--- Java/Algorithm/src/com/programmers/Solution_level1_두개뽑아서더하기.java
+++ Java/Algorithm/src/com/programmers/Solution_level1_두개뽑아서더하기.java
@@ -4,27 +4,25 @@ import java.util.ArrayList;
 
 public class Solution_level1_두개뽑아서더하기 {
 	public static void main(String[] args) {
-		int[] numbers = {2,1,3,4,1}; // 5,0,2,7
+		int[] numbers = { 2, 1, 3, 4, 1 }; // 5,0,2,7
 
 		System.out.println(solution(numbers));
 	}
 
 	public static ArrayList<Integer> solution(int[] numbers) {
-		int answer = 0;
-		
 		ArrayList<Integer> list = new ArrayList<>();
-		
-		for(int i=0;i<numbers.length-1;i++) {
-			for(int j=i+1;j<numbers.length;j++) {
-				if(!list.contains(numbers[i]+numbers[j])) {
+
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (!list.contains(numbers[i] + numbers[j])) {
 					list.add(numbers[i] + numbers[j]);
 				}
 			}
 		}
-		
+
 		list.sort(null); // 오름차순 정렬
 
 		return list;
 	}
-	
+
 }
