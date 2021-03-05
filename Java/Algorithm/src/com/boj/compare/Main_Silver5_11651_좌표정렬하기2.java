cb76838a778b3ceffa45d51cdc29c565d8fad9e9
SungHoAhn <jugiaro95@gmail.com>
2021-03-05 21:18:50 +0900

    :o: boj silver5 11651 좌표정렬하기2

---
+++ Java/Algorithm/src/com/boj/compare/Main_Silver5_11651_좌표정렬하기2.java
@@ -0,0 +1,65 @@
+package com.boj.compare;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_Silver5_11651_좌표정렬하기2 {
+
+	static class Point implements Comparable<Point> {
+		int x, y;
+
+		public Point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Point o) { // this가 나중에 들어온 값
+			if (this.y > o.y) {
+				return 1; // 양수이면 this가 뒤로
+			} else if (this.y < o.y) {
+				return -1; // 음수이면 this가 앞으로
+			} else {
+				if (this.x > o.x) {
+					return 1;
+				} else if (this.x < o.x) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+		}
+
+		@Override
+		public String toString() {
+			return x + " " + y;
+		}
+	}
+
+	static int N;
+	static PriorityQueue<Point> pq;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine());
+
+		pq = new PriorityQueue<Point>();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int inputX = Integer.parseInt(st.nextToken());
+			int inputY = Integer.parseInt(st.nextToken());
+			pq.add(new Point(inputX, inputY));
+		}
+		
+		int size = pq.size();
+		while (size-- > 0) {
+			System.out.println(pq.poll());
+		}
+	}
+}
