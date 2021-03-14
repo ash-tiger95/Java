b3e13a6d1d06a1f740e05bf4d24a5221deffef3c
SungHoAhn <jugiaro95@gmail.com>
2021-03-14 17:57:18 +0900

    :o: boj

---
+++ Java/Algorithm/src/com/programmers/level2/Solution_Hindex.java
@@ -0,0 +1,19 @@
+package com.programmers.level2;
+
+public class Solution_Hindex {
+	public static void main(String[] args) {
+		int[] citations = {3,0,6,1,5};
+		
+		System.out.println(solution(citations));
+	}
+	public static int solution(int[] citations) {
+        int answer = 0;
+        
+        for(int i=0;i<citations.length;i++) {
+        	
+        }
+        	
+        
+        return answer;
+    }
+}
---
+++ Java/Algorithm/src/com/programmers/level2/Solution_주식가격.java
@@ -0,0 +1,24 @@
+package com.programmers.level2;
+
+public class Solution_주식가격 {
+
+	public static void main(String[] args) {
+		int[] prices = { 1, 2, 3, 2, 3 };
+
+		System.out.println(solution(prices));
+	}
+
+	public static int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+
+		for (int i = 0; i < answer.length; i++) {
+			for (int j = i + 1; j < answer.length; j++) {
+				answer[i]++;
+				if (prices[i] > prices[j])
+					break;
+			}
+		}
+		return answer;
+	}
+
+}
