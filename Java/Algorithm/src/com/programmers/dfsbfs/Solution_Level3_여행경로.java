a724c6d36b59e9aee36d1117f9b360c8bd82e2cf
SUNGHO AHN <jugiaro95@gmail.com>
2021-03-20 13:53:02 +0900

    :o: back tracking

---
+++ Java/Algorithm/src/com/programmers/dfsbfs/Solution_Level3_여행경로.java
@@ -0,0 +1,21 @@
+package com.programmers.dfsbfs;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Solution_Level3_여행경로 {
+	public static void main(String[] args) {
+		TreeMap<String, String> ts = new TreeMap<>();
+	       
+        ts.put("aaa", "bbb");
+        ts.put("aaa", "aa");
+        System.out.println(ts);
+	}
+	
+	public static String[] solution(String[][] tickets) {
+        String[] answer = {};
+        
+        
+        return answer;
+    }
+}
