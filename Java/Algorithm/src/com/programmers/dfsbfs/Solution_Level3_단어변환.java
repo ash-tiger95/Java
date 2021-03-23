7b52c89cbb9287e2063e93be2dc43ececea5ea23
SUNGHO AHN <jugiaro95@gmail.com>
2021-03-23 23:15:08 +0900

    :x: 단어변환

--- Java/Algorithm/src/com/programmers/dfsbfs/Solution_Level3_단어변환.java
+++ Java/Algorithm/src/com/programmers/dfsbfs/Solution_Level3_단어변환.java
@@ -1,6 +1,9 @@
 package com.programmers.dfsbfs;
 
 public class Solution_Level3_단어변환 {
+	
+	static int len, answer;
+    static boolean[] checked;
 
 	public static void main(String[] args) {
 		String begin = "hit";
@@ -10,14 +13,39 @@ public class Solution_Level3_단어변환 {
 		System.out.println(solution(begin,target,words));
 	}
 	
-	public static int solution(String begin, String target, String[] words) {
-        int answer = 0;
+	private static int solution(String begin, String target, String[] words) {
+        answer = Integer.MAX_VALUE;
+        len = words.length;
+        checked = new boolean[len];
         
-        for(int i=0;i<words.length;i++) {
-        	
-        }
+        dfs(begin,target, 0, words);
         
-        return answer;
+        return answer == Integer.MAX_VALUE ? 0 : answer;
     }
+
+	private static void dfs(String begin, String target, int count, String[] words) {
+		if(target.equals(begin)) {
+			answer = Math.min(answer, count);
+			return;
+		}
+		
+		for(int i=0;i<len;i++) {
+			if(checking(begin,words[i]) && !checked[i]) {
+				checked[i] = true;
+				dfs(words[i],target,count+1,words);
+				checked[i] = false;
+			}
+		}
+	}
+
+	private static boolean checking(String begin, String string) {
+		int count = 0;
+		
+		for(int i=0;i<begin.length();i++) {
+//			if(begin.charAt(index))
+		}
+		return false;
+	}
+    
 
 }
