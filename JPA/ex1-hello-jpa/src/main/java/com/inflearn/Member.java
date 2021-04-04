cba8953e54a2d3bb7f2a91fd7e0f601d56266321
SUNGHO AHN <jugiaro95@gmail.com>
2021-04-05 03:37:47 +0900

    :o: JPA 시작하기

---
+++ JPA/ex1-hello-jpa/src/main/java/com/inflearn/Member.java
@@ -0,0 +1,31 @@
+package com.inflearn;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "MEMBER") // DB테이블이랑 Mapping
+public class Member {
+	
+	@Id // PK라는 걸 알려준다.
+	private Long id;
+	
+//	@Column(name="username") // DB컬럼이랑 Mapping
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
