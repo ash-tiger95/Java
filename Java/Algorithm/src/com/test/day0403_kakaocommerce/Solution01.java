79476a02dd088f1848f68fb4d97bb92e43c7d55a
SUNGHO AHN <jugiaro95@gmail.com>
2021-04-06 17:07:34 +0900

    :o: Test(Kakao Commerce) solution01

--- Java/Algorithm/src/com/test/day0403_kakaocommerce/Solution01.java
+++ Java/Algorithm/src/com/test/day0403_kakaocommerce/Solution01.java
@@ -1,8 +1,11 @@
 package com.test.day0403_kakaocommerce;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 public class Solution01 {
+
+	// 방법 1. 자료구조 이용
 	public static int solution(int[] gift_cards, int[] wants) {
 		int answer = 0; // 최소
 
@@ -15,9 +18,40 @@ public class Solution01 {
 		for (int i : wants) {
 			if (gc.containsKey(i) && gc.get(i) != 0) {
 				gc.put(i, gc.get(i) - 1);
+
+				if (gc.get(i) == 0) {
+					gc.remove(i);
+				}
+
 				answer++;
 			}
 		}
+
+		return N - answer;
+	}
+
+	// 방법2. 투 포인터
+	public static int solution2(int[] gift_cards, int[] wants) {
+		int answer = 0;
+
+		Arrays.sort(gift_cards);
+		Arrays.sort(wants);
+
+		int left = 0;
+		int right = 0;
+
+		while (left < gift_cards.length && right < wants.length) {
+			if (gift_cards[left] > wants[right]) {
+				right += 1;
+			} else if (gift_cards[left] < wants[right]) {
+				left += 1;
+			} else {
+				left += 1;
+				right += 1;
+				answer += 1;
+			}
+		}
+		int N = gift_cards.length;
 
 		return N - answer;
 	}
