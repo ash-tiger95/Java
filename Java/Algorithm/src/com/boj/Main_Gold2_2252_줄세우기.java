8c0902f093db6b4654b4913d9da8b5edc0c59779
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-04 18:14:07 +0900

    :o: BOJ GOLD4 1171 집합의표현 Union-Find

---
+++ Java/Algorithm/src/com/boj/Main_Gold2_2252_줄세우기.java
@@ -0,0 +1,27 @@
+package com.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 위상정렬
+ * 
+ * @author jugia
+ *
+ */
+public class Main_Gold2_2252_줄세우기 {
+
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); // 학생 수, 1 <= N <= 32,000
+		M = Integer.parseInt(st.nextToken()); // 비교 횟수, 1 <= M <= 100,000
+		
+		
+	}
+}
