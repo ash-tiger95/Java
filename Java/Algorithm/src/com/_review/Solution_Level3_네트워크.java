8e3ca1d8ac958ee713af1bfd5819081e6a4e6718
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-06 15:16:40 +0900

    :o: PROGRAMMERS LEVEL3 네트워크 DFS

---
+++ Java/Algorithm/src/com/_review/Solution_Level3_네트워크.java
@@ -0,0 +1,42 @@
+package com._review;
+
+public class Solution_Level3_네트워크 {
+
+	static int Ans;
+	static boolean[] visited;
+
+	public static int solution(int n, int[][] computers) {
+		Ans = 0;
+		visited = new boolean[n]; // 정점 방문 처리
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) { // 방문하지 않은 정점인 경우
+				visited[i] = true;
+				dfs(i, n, computers); // 깊이 우선 탐색
+				Ans++;
+			}
+		}
+
+		return Ans;
+	}
+
+	public static void dfs(int start, int n, int[][] computers) { // start(열)를 기준으로 행 검사
+		for (int j = 0; j < n; j++) {
+			if (start == j) {
+				continue;
+			}
+
+			if (!visited[j] && computers[start][j] == 1) {
+				visited[j] = true; // 방문 처리 후 DFS
+				dfs(j, n, computers);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[][] computers = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+		System.out.println(solution(n, computers));
+	}
+
+}
