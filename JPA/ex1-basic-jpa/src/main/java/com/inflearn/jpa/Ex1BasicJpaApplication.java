73ef072f6d3273b6325078efeec6179d4ade73bd
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-11 19:15:44 +0900

    :o: Inflearn JPA 이론 4-1

---
+++ JPA/ex1-basic-jpa/src/main/java/com/inflearn/jpa/Ex1BasicJpaApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1BasicJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1BasicJpaApplication.class, args);
+	}
+
+}
---
+++ JPA/ex1-basic-jpa/src/test/java/com/inflearn/jpa/Ex1BasicJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1BasicJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
