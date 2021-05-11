002b9497403f3406ba87bd52670331fa5911bc7f
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-11 20:01:10 +0900

    update

---
+++ Java/Algorithm/src/com/_review/Main_Silver2_1713_후보추천하기.java
@@ -0,0 +1,7 @@
+package com._review;
+
+public class Main_Silver2_1713_후보추천하기 {
+	public static void main(String[] args) {
+		
+	}
+}
--- Java/Algorithm/src/com/boj/search/binarysearch/Main_Silver4_1920_수찾기.java
+++ Java/Algorithm/src/com/boj/search/binarysearch/Main_Silver4_1920_수찾기.java
@@ -1,4 +1,4 @@
-package com._review.day210511;
+package com.boj.search.binarysearch;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,7 +17,7 @@ public class Main_Silver4_1920_수찾기 {
 
 		// 입력
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 1 <= N <= 100,000
+		N = Integer.parseInt(st.nextToken()); // N개의 정수 입력, 1 <= N <= 100,000
 
 		map = new int[N];
 		st = new StringTokenizer(br.readLine());
@@ -28,12 +28,16 @@ public class Main_Silver4_1920_수찾기 {
 		Arrays.sort(map); // 이분 탐색을 위한 정렬
 
 		// 입력
-		M = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine()); // 찾아야 되는 정수 개수, 1 <= M <= 100,000
 		st = new StringTokenizer(br.readLine());
 
-		for (int t = 0; t < M; t++) {
+		for (int t = 0; t < M; t++) { // 684ms
 			int m = Integer.parseInt(st.nextToken());
-
+			
+			// 참고, 1332ms (재귀)
+			// System.out.println(binarySearch(map, m) == true ? 1 : 0);
+			
+			// 내 풀이
 			int left = 0;
 			int right = N - 1;
 			int mid;
@@ -63,6 +67,28 @@ public class Main_Silver4_1920_수찾기 {
 		} // for M
 
 		System.out.println(sb);
+		
+	}
+	
+	private static boolean binarySearch(int[] arr, int key) { // Clean Code
+		int left = 0;
+		int right = arr.length - 1;
+		int mid;
+
+		while (left <= right) {
+			mid = (left + right) / 2;
+
+			if (key == arr[mid]) {
+				return true;
+			}
+
+			if (key < arr[mid]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return false;
 	}
 
 }
