42a80784600a2416ba58d4754b2d98cf1640fd8b
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-17 14:43:10 +0900

    :o: [BOJ Silver3 5397 키로거] LinkedList 사용

---
+++ Java/Algorithm/src/com/boj/datastructure/linkedlist/Main_Silver3_5397_키로거.java
@@ -0,0 +1,61 @@
+package com.boj.datastructure.linkedlist;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main_Silver3_5397_키로거 {
+
+	static int T;
+	static String in;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		T = Integer.parseInt(st.nextToken()); // 테스트 케이스
+
+		for (int t = 0; t < T; t++) {
+			in = br.readLine();
+			LinkedList<Character> temp = new LinkedList<>();
+			int cursor = 0;
+
+			for (int i = 0; i < in.length(); i++) {
+
+				if (in.charAt(i) == '<') { // 1. 커서 이동
+					cursor--;
+					if (cursor < 0) { // 커서의 최솟값은 0
+						cursor = 0;
+					}
+					
+				} else if (in.charAt(i) == '>') { // 2. 커서 이동
+					cursor++;
+					if (cursor > temp.size()) { // 커서의 최댓값은 temp.size()
+						cursor = temp.size();
+					}
+					
+				} else if (in.charAt(i) == '-') { // 3. 백 스페이스: 커서 앞 문자 삭제
+					if (temp.size() == 0 || cursor == 0) { // 3-1) 삭제할 문자가 없는 경우 || 커서가 맨 앞에 있을 때
+						continue;
+					} else { // 3-2) 삭제할 문자가 있을 때
+						temp.remove(--cursor);
+					}
+
+				} else {
+					temp.add(cursor++, in.charAt(i));
+				}
+
+			}
+
+			for(Character c : temp) {
+				sb.append(c);
+			}
+			sb.append("\n");
+
+		} // for T
+		System.out.print(sb);
+	}
+}
---
+++ Java/Algorithm/src/com/boj/datastructure/stack/Main_Silver3_5397_키로거.java
@@ -0,0 +1,59 @@
+package com.boj.datastructure.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_Silver3_5397_키로거 {
+	
+	static int T;
+	static String in;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		T = Integer.parseInt(st.nextToken()); // 테스트 케이스
+
+		for (int t = 0; t < T; t++) {
+			in = br.readLine();
+			// 커서를 중심으로 앞부분은 front 스택에, 뒷부분은 back 스택에 넣는다.
+			Stack<Character> front = new Stack<>();
+			Stack<Character> back = new Stack<>();
+
+			for (int i = 0; i < in.length(); i++) {
+
+				if (in.charAt(i) == '<') { // 1. 커서 이동
+					if(!front.isEmpty()) {
+						back.push(front.pop());
+					}
+				} else if (in.charAt(i) == '>') { // 2. 커서 이동
+					if(!back.isEmpty()) {
+						front.push(back.pop());
+					}
+				} else if (in.charAt(i) == '-') { // 3. 백 스페이스: 커서 앞 문자 삭제
+					if(!front.isEmpty()) {
+						front.pop();
+					}
+				} else {
+					front.push(in.charAt(i));
+				}
+
+			}
+
+			while(!front.isEmpty()) {
+				back.push(front.pop());
+			}
+			
+			while(!back.isEmpty()) {
+				sb.append(back.pop());
+			}
+			sb.append("\n");
+
+		} // for T
+		System.out.print(sb);
+	}
+}
