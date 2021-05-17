fe23900110080e7aea721a6dd5c49e8d6cf0012c
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-17 15:13:52 +0900

    :o: [BOJ Silver1 5397 현수막] BFS

---
+++ Java/Algorithm/src/com/boj/search/bfs/Main_Silver1_14716_현수막.java
@@ -0,0 +1,79 @@
+package com.boj.search.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 풀이) BFS 기본 문제
+ * 
+ * @author jugia
+ *
+ */
+public class Main_Silver1_14716_현수막 {
+
+	static int R, C, Ans;
+	static int[][] map;
+	static boolean[][] visited;
+	static int dirs[][] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		Ans = 0;
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new int[R][C];
+		visited = new boolean[R][C];
+
+		for (int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] == 1 && !visited[i][j]) { // 방문하지 않고 1인 점 BFS
+					visited[i][j] = true;
+					bfs(i, j);
+					Ans++;
+				}
+			}
+		}
+
+		System.out.println(Ans);
+	}
+
+	private static void bfs(int cy, int cx) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] { cy, cx });
+
+		while (!q.isEmpty()) {
+			int[] cp = q.poll();
+
+			for (int d = 0; d < 8; d++) {
+				int ny = cp[0] + dirs[d][0];
+				int nx = cp[1] + dirs[d][1];
+
+				if (!boundary(ny, nx)) {
+					continue;
+				}
+
+				if (!visited[ny][nx] && map[ny][nx] == 1) {
+					visited[ny][nx] = true;
+					q.add(new int[] { ny, nx });
+				}
+			}
+		}
+	}
+
+	private static boolean boundary(int ny, int nx) {
+		return ny >= 0 && ny < R && nx >= 0 && nx < C;
+	}
+}
