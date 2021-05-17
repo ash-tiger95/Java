a82e8ad46c4fcf611c739b38e58859faf6fb0453
SUNGHO AHN <jugiaro95@gmail.com>
2021-05-18 08:50:39 +0900

    :o: [BOJ Silver1 7569 토마토] 3차원 BFS

--- Java/Algorithm/src/com/boj/math/euclidean/Main_Silver5_2609_최대공약수와최소공배수.java
+++ Java/Algorithm/src/com/boj/math/euclidean/Main_Silver5_2609_최대공약수와최소공배수.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 풀이) 유클리드 호제법
- * 최대공약수 -> 유클리드 호제법
- * 최소공배수 -> a * b / 최대공약수
+ * 풀이) 유클리드 호제법 최대공약수 -> 유클리드 호제법 최소공배수 -> a * b / 최대공약수
  * 
  * @author jugia
  *
@@ -16,27 +14,27 @@ import java.util.StringTokenizer;
 public class Main_Silver5_2609_최대공약수와최소공배수 {
 
 	static int a, b;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		a = Integer.parseInt(st.nextToken());
 		b = Integer.parseInt(st.nextToken());
-		
-		int gcd = euclidean(a,b); // 최대공약수
+
+		int gcd = euclidean(a, b); // 최대공약수
 		int lcm = a * b / gcd; // 최소공배수
-		
+
 		System.out.println(gcd);
 		System.out.println(lcm);
 	}
-	
+
 	private static int euclidean(int x, int y) {
-		if(y == 0) {
+		if (y == 0) {
 			return x;
 		} else {
-			return euclidean(y, x%y);
+			return euclidean(y, x % y);
 		}
 	}
-	
+
 }
---
+++ Java/Algorithm/src/com/boj/search/bfs/Main_Silver1_7569_토마토.java
@@ -0,0 +1,105 @@
+package com.boj.search.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 풀이) 3차원 BFS
+ * 
+ * @author jugia
+ *
+ */
+public class Main_Silver1_7569_토마토 {
+
+	static int R, C, H, total, Ans;
+	static int[][][] box;
+	static boolean[][][] visited;
+	static Queue<int[]> q;
+
+	static int[][] dirs = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 1 }, { 0, 0, -1 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		Ans = 0;
+		C = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		total = H * R * C;
+
+		box = new int[H][R][C];
+		visited = new boolean[H][R][C];
+		q = new LinkedList<>();
+
+		for (int h = 0; h < H; h++) {
+			for (int r = 0; r < R; r++) {
+				st = new StringTokenizer(br.readLine());
+				for (int c = 0; c < C; c++) {
+					box[h][r][c] = Integer.parseInt(st.nextToken());
+
+					if (box[h][r][c] == 1) { // 1: 익은 토마토, 0: 익지 않은 토마토
+						q.add(new int[] { h, r, c });
+						visited[h][r][c] = true;
+						total--;
+						
+					} else if (box[h][r][c] == -1) { // 토마토가 없는 칸
+						total--;
+					}
+				}
+			}
+		}
+
+		if (total == 0) { // 저장될 때부터 모든 토마토가 익은 상태
+			System.out.println(0);
+		} else {
+			while (true) {
+				bfs();
+				Ans++;
+
+				if (total == 0) { // 토마토가 모두 익은 상황
+					System.out.println(Ans);
+					break;
+				}
+				
+				if(total != 0 && q.isEmpty()) { // 토마토가 모두 익지 못하는 상황
+					System.out.println(-1);
+					break;
+				}
+			}
+
+		}
+	}
+
+	private static void bfs() {
+		int size = q.size();
+
+		for (int i = 0; i < size; i++) {
+			int[] cp = q.poll();
+
+			for (int d = 0; d < 6; d++) {
+				int nz = cp[0] + dirs[d][0];
+				int ny = cp[1] + dirs[d][1];
+				int nx = cp[2] + dirs[d][2];
+
+				if (!boundary(nz, ny, nx)) {
+					continue;
+				}
+
+				if (!visited[nz][ny][nx] && box[nz][ny][nx] == 0) {
+					q.add(new int[] { nz, ny, nx });
+					visited[nz][ny][nx] = true;
+					total--;
+				}
+			}
+		}
+	}
+
+	private static boolean boundary(int nz, int ny, int nx) {
+		return nz >= 0 && nz < H && ny >= 0 && ny < R && nx >= 0 && nx < C;
+	}
+}
